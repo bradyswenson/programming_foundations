a17e7d9a86a1857b1dd0b0cdbb7112c4faea5f40
Brady Swenson <bradyswenson@gmail.com>
2015-12-22 13:53:19 -0600

    saving progress

---
+++ lesson_2/psuedo_code.rb
@@ -0,0 +1,36 @@
+=begin
+  
+For example, write out pseudo-code (both casual and formal) that does the following:
+
+1. a method that returns the sum of two integers
+2. a method that takes an array of strings, and returns a string that is all those strings concatenated together
+3. a method that takes an array of integers, and returns a new array with every other element
+ 
+-------------------------
+
+casual psuedo code 
+
+1. 
+
+take two integer inputs
+multiply one integer by the other
+
+START
+
+GET two integers
+SET integer1 and integer2
+SET result = sum of integer1 and integer2
+PRINT results
+
+END
+
+2. 
+
+START
+
+GET array of integers
+
+
+
+=end
+
