c7d04784e33d791e1e17a8b06060bbdb372be6d6
Brady Swenson <bradyswenson@gmail.com>
2016-01-04 17:02:43 -0600

    easy 3 done

---
+++ lesson_3/easy_3.rb
@@ -0,0 +1,54 @@
+puts 'Question 1:'
+
+flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+p flintstones
+
+
+puts 'Question 2:'
+
+flintstones << 'Dino'
+
+p flintstones
+
+
+puts 'Question 3:'
+
+flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+flintstones.push('Dino', 'Hoppy')
+
+p flintstones
+
+
+puts 'Question 4:'
+
+advice = "Few things in life are as important as house training your pet dinosaur."
+
+advice.slice!(0, 39)
+
+puts advice
+
+
+puts 'Question 5:'
+
+statement = "The Flintstones Rock!"
+
+puts statement.count('t')
+
+
+puts 'Question 6:'
+
+title = "Flintstone Family Members"
+
+puts title.center(40)
+
+
+puts 'Question 7:'
+
+
+
+
+
